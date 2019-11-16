a47a274aa579a1179c5153e710311ec93e375033
Mor NDIAYE <mor.ndiaye@calmedica.fr>
2019-11-17 00:43:44 +0100

    Updates

---
+++ precache-manifest.a66fe3fde62ec9763ae640735cde0777.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3f3282785bf58392ae7541a2ce84bcc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "45673b4b72e55e1f0588",
+    "url": "/monsters-rolodex/static/css/main.691113ee.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "45673b4b72e55e1f0588",
+    "url": "/monsters-rolodex/static/js/main.9b9c0090.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
